a6a434a584937773c47a95de6948e42f28e3c38b
Aadity Aabha Patel <aadity.patelthoughtworks.com@Aaditys-MacBook-Pro.local>
2022-12-25 20:01:44 +0530

    Initial Commit.

---
+++ src/main/java/com/projects/InMemoryCacheImplementation/InMemoryCacheImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.projects.InMemoryCacheImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryCacheImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemoryCacheImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/InMemoryCacheImplementation/InMemoryCacheImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.InMemoryCacheImplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemoryCacheImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
